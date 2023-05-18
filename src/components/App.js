f6ba2d5dc436fb65639fefa97059410147c2c7f5
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-05-18 12:48:28 +0530

    submitting code-Thu May 18 2023 12:48:27 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name,setName] = useState();
+
+
   return (
     <div>
-       <input type="text" />
+      <p>Enter your name:</p><br/><br/>
+       <input type="text" onChange={(e) => setName(e.target.value) } />
+
+       <p>Hello {name}!</p>
     </div>
   )
 }
